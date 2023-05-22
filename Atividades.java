d2acbf1d60e439f28e87e7b9697d56a1b072ce41
Gustavosousaz <gbw2920@gmail.com>
2023-05-22 20:01:14 -0300

    22/05 termino da att

--- Atividades.java
+++ Atividades.java
@@ -74,10 +74,10 @@ public class Atividades {
         // Sortear um numero de 0 a 1000
         int nDigitado = 0;
         int tentativas = 1;
-        int nSorteado = (rd.nextInt(5));
+        int nSorteado = (rd.nextInt(1000));
         boolean acertou = false;
         while (acertou == false) {
-            System.out.println("Digite o numero novamente ");
+            System.out.println("Digite um numero até acertar! ");
             nDigitado = sc.nextInt();
             if (nSorteado == nDigitado) {
                 System.out.println("Parabéns! Você acertou o número sorteado em " + tentativas + " tentativa.");
@@ -89,6 +89,49 @@ public class Atividades {
             }
 
             tentativas++;
+        }
+    }
+
+    // atividade3 - vetor
+    public void atividade3() {
+        // criar um vetor 100-1000 itens
+        int vetor[] = new int[rd.nextInt(900) + 100];
+        // preencher o vetor
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = rd.nextInt(100) + 1;
+        }
+        // procurar os nº Impares
+        int contImparNaPar = 0;
+        int contParNaImpar = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 1) {// resto == 1
+                System.out.println(vetor[i]);
+                // se esta na cada par
+                if (i % 2 == 1) {// casa par
+                    contImparNaPar++;
+                }
+            }
+        }
+        // procurar os nº Pares
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 0) {// resto == 0
+                System.out.println(vetor[i]);
+                if (i % 2 == 0) {// casa Impar
+                    contParNaImpar++;
+                }
+            }
+        }
+        // contar os Impares na casa Par
+        for (int i = 1; i < vetor.length; i += 2) {
+            if (vetor[i] % 2 == 1) {
+                contImparNaPar++;
+            }
+        }
+        // contar os Pares na cas Impar
+        for (int i = 0; i < vetor.length; i += 2) {
+            if (vetor[i] % 2 == 0) {
+                contParNaImpar++;
+            }
         }
     }
 }
